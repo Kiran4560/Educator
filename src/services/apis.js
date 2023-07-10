477cdef899c1e08a0513b9f96a9f476f8dd6d748
Priyanshu03M <97392355+Priyanshu03M@users.noreply.github.com>
2023-07-11 02:49:46 +0530

    Instructor section added

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,4 @@
-//const BASE_URL = process.env.REACT_APP_BASE_URL
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = process.env.REACT_APP_BASE_URL
 
 // AUTH ENDPOINTS
 export const endpoints = {
@@ -14,6 +13,7 @@ export const endpoints = {
 export const profileEndpoints = {
   GET_USER_DETAILS_API: BASE_URL + "/profile/getUserDetails",
   GET_USER_ENROLLED_COURSES_API: BASE_URL + "/profile/getEnrolledCourses",
+  GET_INSTRUCTOR_DATA_API: BASE_URL + "/profile/instructorDashboard",
 }
 
 // STUDENTS ENDPOINTS
